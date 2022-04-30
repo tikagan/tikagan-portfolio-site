103828844ee60e08cd2302a6a61dc6b68680ab62
tikagan <tikagan@Marvin.local>
2022-04-30 15:43:08 -0400

    adds main page content and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='App'>
+      <div className='content'>
+        <div className='kritika-portrait'></div>
+        <h1 className='hi-im-kritika'>Hi I'm Kritika!</h1>
+        <p className='about-me-text'>
+          I'm a full-stack developer with robust range of skills. I'm looking for new opportunities that take advantage of my resourcefulness, curiosity, and passion while offering mentorship, community, and challenging opportunities.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
