07f24c356f453f7ac260589d6877c4cf98a4275a
tikagan <tikagan@Marvin.local>
2022-05-10 16:43:00 -0400

    asset folder restructure and link corrections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {ReactComponent as GithubLogo} from '../assets/GitHub-Mark.svg'
-import {ReactComponent as LinkedinLogo} from '../assets/LinkedIn_Logo.svg'
-import {ReactComponent as Email} from '../assets/Envelope.svg'
+import {ReactComponent as GithubLogo} from '../assets/icons/GitHub-Mark.svg'
+import {ReactComponent as LinkedinLogo} from '../assets/icons/LinkedIn_Logo.svg'
+import {ReactComponent as Email} from '../assets/icons/Envelope.svg'
 
 function Footer() {
     return (
